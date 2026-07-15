cc129ebe6695c5784bdeab4171c95ce1710d47ac
Arpit Chauhan <88385002+ArpitChauhan1501@users.noreply.github.com>
2026-07-16 03:41:11 +0530

    Refactor Config class to use environment variable

--- config.py
+++ config.py
@@ -1,6 +1,11 @@
+import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = "sqlite:///trekking.db"
     SECRET_KEY = "63926e0f2b6d6d60cae441b2df4eb9a4"
+
+    SQLALCHEMY_DATABASE_URI = os.getenv(
+        "DATABASE_URL",
+        "sqlite:///trekking.db"
+    )
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    
